2693c4c1d30861c52681404c750d7431f9a75883
Pomelo <liangrenhong2017@gmail.com>
2023-11-26 15:00:15 +0000

    Update 3 files

--- src/components/Mapbox/components/Lines/index.jsx
+++ src/components/Mapbox/components/Lines/index.jsx
@@ -1,22 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getLocation, getMarker } from "../../utils";
 import locations from "../../data/locations.json";
+import { Button, Select } from "@arco-design/web-react";
 
 export default ({ map }) => {
+  const [show, setShow] = useState(false);
+  const [layer, setLayer] = useState("");
+
   const addLines = (map) => {
     const lines = locations.features.map((item) => item.geometry.coordinates);
     console.log(lines);
-    map.addSource("route", {
-      type: "geojson",
-      data: {
-        type: "Feature",
-        properties: {},
-        geometry: {
-          type: "LineString",
-          coordinates: lines,
+    if (!layer) {
+      map.addSource("route", {
+        type: "geojson",
+        data: {
+          type: "Feature",
+          properties: {},
+          geometry: {
+            type: "LineString",
+            coordinates: lines,
+          },
         },
-      },
-    });
+      });
+    }
     map.addLayer({
       id: "route",
       type: "line",
@@ -30,10 +36,32 @@ export default ({ map }) => {
         "line-width": 8,
       },
     });
+    setLayer("route");
   };
   useEffect(() => {
-    map && addLines(map);
-  }, [map]);
+    if (!show) {
+      map && map.removeLayer("route");
+    } else {
+      if (!layer) {
+        map && !layer && addLines(map);
+      } else {
+        map && map.removeLayer("route");
+      }
+    }
+  }, [map, show]);
 
-  return null;
+  return (
+    <Button
+      onClick={() => setShow(!show)}
+      style={{
+        position: "fixed",
+        zIndex: 1000,
+        right: 20,
+        top: 100,
+      }}
+      type="primary"
+    >
+      显示航线
+    </Button>
+  );
 };
--- src/components/Mapbox/components/Locations/index.jsx
+++ src/components/Mapbox/components/Locations/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { getLocation, getMarker } from "../../utils";
 import locations from "../../data/locations.json";
+import mapboxgl from "mapbox-gl";
 
 export default ({ map }) => {
   const addLocations = (map) => {
@@ -31,6 +32,25 @@ export default ({ map }) => {
         });
       },
     );
+
+    map.on("click", "points", (e) => {
+      // Copy coordinates array.
+      const coordinates = e.features[0].geometry.coordinates.slice();
+      const description = e.features[0].properties.description || "";
+
+      // Ensure that if the map is zoomed out such that multiple
+      // copies of the feature are visible, the popup appears
+      // over the copy being pointed to.
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
+
+      description &&
+        new mapboxgl.Popup()
+          .setLngLat(coordinates)
+          .setHTML(description)
+          .addTo(map);
+    });
   };
 
   useEffect(() => {
